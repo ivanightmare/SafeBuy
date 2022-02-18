31bf43f3e49e6c18bfd664511fdcb25c6a0330ff
Ivan Xie Ng (irx521) <irx521@mail.usask.ca>
2022-02-17 21:46:02 -0600

    init app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const passport = require('passport');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+const monogoose = require('mongoose');
+
