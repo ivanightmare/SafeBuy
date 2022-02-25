f24046423e27c80dfce49402e9a603e76e8eea03
Ivan Xie Ng (irx521) <irx521@mail.usask.ca>
2022-02-24 21:26:26 -0600

    updated module exports of User.js

--- models/User.js
+++ models/User.js
@@ -17,7 +17,6 @@ const UserSchema = new mongoose.Schema({
     password: {
         type = String,
         required: true
-
     },
     phone: {
         type = String,
@@ -46,7 +45,6 @@ const UserSchema = new mongoose.Schema({
     date: {
         type: String,
         require: true
-
     },
     profile: {
         type: Image,
@@ -56,7 +54,6 @@ const UserSchema = new mongoose.Schema({
         type: String,
         default: 'user',
         enum: ['user','employee','manager','boss']
-
     },
     status: {
         type: String,
@@ -72,4 +69,105 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
+
+module.exports.getUser = (callback, limit) => {
+    User.find(callback).limit(limit);
+}
+
+module.exports.getUserById = (id, callback) => {
+    User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = (username, callback) => {
+    const query = {username: username};
+    User.findOne(query, callback);
+}
+
+module.exports.updateUser = (id, user, options, callback) => {
+    var query = {_id: id};
+    var update = {
+        name: user.name,
+        username: user.username,
+        email: user.email,
+        password: user.password,
+        phone: user.phone,
+        address: user.address,
+        city: user.city,
+        state: user.state,
+        zip: user.zip,
+        photoID: user.photoID,
+        date: user.date,
+        profile: user.profile,
+        role: user.role,
+        status: user.status
+    }
+    User.findOneAndUpdate(query, update, options, callback);
+}
+
+module.exports.removeUser = (id, callback) => {
+    var query = {_id: id};
+    User.remove(query, callback);
+}
+
+module.exports.addUser = (user, callback) => {
+    User.create(user, callback);
+}
+
+module.exports.getUserByEmail = (email, callback) => {
+    const query = {email: email};
+    User.findOne(query, callback);
+}
+
+module.exports.getUserByPhone = (phone, callback) => {
+    const query = {phone: phone};
+    User.findOne(query, callback);
+}
+
+module.exports.getUserByAddress = (address, callback) => {
+    const query = {address: address};
+    User.findOne(query, callback);
+}
+
+module.exports.getUserByCity = (city, callback) => {
+    const query = {city: city};
+    User.findOne(query, callback);
+}
+
+module.exports.getUserByState = (state, callback) => {
+    const query = {state: state};
+    User.findOne(query, callback);
+}
+
+module.exports.getUserByZip = (zip, callback) => {
+    const query = {zip: zip};
+    User.findOne(query, callback);
+}
+
+module.exports.getUserByPhotoID = (photoID, callback) => {
+    const query = {photoID: photoID};
+    User.findOne(query, callback);
+}
+
+module.exports.getUserByDate = (date, callback) => {
+    const query = {date: date};
+    User.findOne(query, callback);
+}
+
+module.exports.getUserByRole = (role, callback) => {
+    const query = {role: role};
+    User.findOne(query, callback);
+}
+
+module.exports.getUserByStatus = (status, callback) => {
+    const query = {status: status};
+    User.findOne(query, callback);
+}
+
+module.exports.getUserByCreatedOn = (createdOn, callback) => {
+    const query = {createdOn: createdOn};
+    User.findOne(query, callback);
+}
+
+
+
 
