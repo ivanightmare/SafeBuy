cc42aaeae504b66ffc7d95d8b3a5acc0d8fa7639
Ivan Xie Ng (irx521) <irx521@mail.usask.ca>
2022-02-24 21:47:02 -0600

    init app.js

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express');
-const app = express();
+
 
 const passport = require('passport');
 const session = require('express-session');
 const flash = require('connect-flash');
 
-const monogoose = require('mongoose');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const Product = require('./models/Product');
+const Order = require('./models/Order');
+const Cart = require('./models/Cart');
+const CartItem = require('./models/CartItem');
+const OrderItem = require('./models/OrderItem');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+
 
 
 
