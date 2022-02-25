bb83a850c7f9f7cb8921c3873c5706fc65e297dc
Ivan Xie Ng (irx521) <irx521@mail.usask.ca>
2022-02-24 21:12:25 -0600

    new Schema for user

--- models/User.js
+++ models/User.js
@@ -6,6 +6,10 @@ const UserSchema = new mongoose.Schema({
         type = String,
         required = true
     },
+    username:{
+        type: String,
+        required: true
+    },
     email: {
         type = String,
         required: true
@@ -15,10 +19,38 @@ const UserSchema = new mongoose.Schema({
         required: true
 
     },
+    phone: {
+        type = String,
+        required: true
+    },
+    address: {
+        type = String,
+        required: true
+    },
+    city: {
+        type = String,
+        required: true
+    },
+    state: {
+        type = String,
+        required: true
+    },
+    zip: {
+        type = String,
+        required: true
+    },
+    photoID: {
+        type = String,
+        required: true
+    },
     date: {
         type: String,
         require: true
 
+    },
+    profile: {
+        type: Image,
+        required: true
     },
     role: {
         type: String,
@@ -26,6 +58,16 @@ const UserSchema = new mongoose.Schema({
         enum: ['user','employee','manager','boss']
 
     },
+    status: {
+        type: String,
+        default: 'active',
+        enum: ['active','inactive']
+    },
+    createdOn: {
+        type = Date,
+        default: Date.now
+    },
+
 });
 
 const User = mongoose.model('User', UserSchema);
